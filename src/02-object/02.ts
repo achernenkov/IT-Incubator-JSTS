e0269faa962b01e65dc6d21e645278338e4c85c9
achernenkov <jardagorng@gmail.com>
2020-12-12 19:00:31 +0300

    Create object + type. l2.

---
+++ src/02-object/02.ts
@@ -0,0 +1,77 @@
+// Создание с помощью {} - создание с помощью синтаксиса литерала объекта.
+// Ключ - это строка, но удобства не береться в скобки.
+
+// Даемный явный тип объекту, вместо неявного.
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    tehnologies: Array<TehnologiesType>
+    'Мое имя': string
+    'My name': string
+    'Test 3': Test3Type
+}
+
+type AddressType = {
+    streetTitle: string
+    city: AddressCityType
+}
+
+type AddressCityType = {
+    title: string
+    country: string
+}
+
+type TehnologiesType = {
+    id: number
+    title: string
+}
+
+type Test3Type = {
+    'Name Key': NameKeyType
+}
+
+type NameKeyType = {
+    'Name Key 2': boolean
+    'Name Key 3': boolean
+}
+
+const student: StudentType = {
+    id: 1,
+    "name": 'Andrey',
+    age: 23,
+    isActive: true,
+    address: {
+        streetTitle: 'Surganova',
+        city: {
+            title: 'Minsk',
+            country: 'Belarus'
+        }
+    },
+    tehnologies: [
+        {id:1, title: 'HTML'},
+        {id:2, title: 'CSS'},
+        {id:3, title: 'REACT'},
+    ],
+    'Мое имя': 'Тест',
+    'My name': 'test 2',
+    'Test 3':{
+        'Name Key':{
+            'Name Key 2': true,
+            'Name Key 3': false
+        }
+    }
+}
+
+// Как обращаться к "неправильным" свойствам.
+console.log(student["Мое имя"])
+console.log(student["My name"])
+
+console.log(student['Test 3']["Name Key"]["Name Key 2"])
+
+console.log(student.tehnologies[2].title) // React получили.
+
+
+export default student;
